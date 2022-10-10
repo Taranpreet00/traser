a8990de551d4d726d158b85929a0a4dc4d2dcb9b
Taranpreet Singh <taranpreetsingh308@gmail.com>
2022-10-10 19:07:09 +0530

    add views and paths for signin and signup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile = function(req, res){
         title: "User Profile"
     })
 }
+
+module.exports.signup = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Signup"
+    });
+}
+
+module.exports.signin = function(req, res){
+    return res.render('user_sign_in', {
+        title: "Signin"
+    })
+}
+
+module.exports.submitSignin = function(req, res){
+    return res.end("<h1> Signin request Successfully submitted</h1>");
+}
+
+module.exports.submitSignup = function(req, res){
+    return res.end("<h1>Signup request Successfully Submitted</h1>");
+}
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 router.get('/profile', usersController.profile);
-
+router.get('/signup', usersController.signup);
+router.get('/signin', usersController.signin);
+router.post('/signin/create-session', usersController.submitSignin);
+router.post('/signup/create', usersController.submitSignup);
 
 
 module.exports = router;
