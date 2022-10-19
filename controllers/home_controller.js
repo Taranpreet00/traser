05333246cbe0fc4ecb32a87f23f83d9e7c25a283
Taranpreet Singh <taranpreetsingh308@gmail.com>
2022-10-19 10:32:28 +0530

    create posts schema, link it to user and view on homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,22 @@
+const Post = require('../models/post');
+const User = require('../models/user');
+
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
+    
+    // Post.find({},function(err, posts){
+    //     console.log(posts);
+    //     return res.render('home', {
+    //         title: "Home",
+    //         posts: posts
+    //     });
+    // })
+
+    Post.find().populate('user').exec(function(err, posts){
+        if(err){console.log(err); return;}
+        return res.render('home', {
+            title: "Home",
+            posts: posts
+        })
     })
+    
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,20 @@
-module.exports.post = function(req, res){
-    res.end('<h1>Posts will be visible here</h1>');
+const Post = require('../models/post');
+
+module.exports.create = function(req, res){
+    if(req.isAuthenticated()){
+        const curruser = req.user._id;
+        Post.create({
+            content: req.body.content,
+            user: curruser
+        }, function(err, post){
+            if(err){
+                console.log('Error in creating new post');
+                return;
+            }
+            return res.redirect('/users/profile');
+        });
+    }
+    else{
+        return res.redirect('/users/signin');
+    }
 }
