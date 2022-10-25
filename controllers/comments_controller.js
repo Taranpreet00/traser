f5ba7bad75910ed2c27c9b04cdf315aa4966871c
Taranpreet Singh <taranpreetsingh308@gmail.com>
2022-10-25 22:34:43 +0530

    add delete posts and delete comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -19,5 +19,27 @@ module.exports.create = function(req, res){
             })
         }
     })
-    
+}
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(req.user.id == comment.user){
+            // Post.findById(comment.post, function(err, post){
+            //     let commentIndex = post.comments.findIndex(currcomment => currcomment == req.params.id);
+            //     if(commentIndex != -1){
+            //         post.comments.splice(commentIndex, 1);
+            //         post.save();
+            //         // contactList.splice(contactIndex, 1);
+            //     }
+            // });
+            let postId = comment.post;
+            comment.remove();
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     // if(req.isAuthenticated()){
@@ -17,4 +18,21 @@ module.exports.create = function(req, res){
     // else{
     //     return res.redirect('/users/signin');
     // }
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(err || !post){
+            return res.redirect('back');
+        }
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
+            });
+        }else{
+            res.redirect('back');
+        }
+    });
 }
